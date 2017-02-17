f1b79f5b3f78a5da79f27047191ed5b3f287abbd
jjen6894 <jack_6894@hotmail.com>
2017-02-17 12:09:57 +0000

    nice for-loop not using camelCase anymore, just be consistent.

--- ContinueBreakElse.py
+++ ContinueBreakElse.py
@@ -11,11 +11,19 @@ for item in shopping_list:
         break
     print("Buy " + item)
 print("-------------------")
-meal = ["egg", "bacon", "spam", "sausages"]
+meal = ["egg", "bacon", "tomato", "sausages"]
+
+nasty_food_item = ''
 
 for item in meal:
     if item == 'spam':
         nasty_food_item = item
-        break 
+        break
+else:
+    nasty_food_item = ''
+    print("I'll have a plate of that, then, please")
 if nasty_food_item:
     print("Can't I have anything without spam in it")
+
+
+
