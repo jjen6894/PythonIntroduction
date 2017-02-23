5618355aa36da9099734b43fc7bc09b5470fe87d
jjen6894 <jack_6894@hotmail.com>
2017-02-23 14:31:30 +0000

    accidently changed a file on list challenge, iterator mini challenge complete

--- Iterators/iterators.py
+++ Iterators/iterators.py
@@ -1,18 +1,31 @@
-string = '1234567890'
+# string = '1234567890'
+#
+# for char in string:
+#     print(char)
+#
+# print('-------------')
+# string_iterated = iter(string)
+# print(string_iterated)
+# print(next(string_iterated))
+# print(next(string_iterated))
+# print(next(string_iterated))
+# print(next(string_iterated))
+# print(next(string_iterated))
+# print(next(string_iterated))
+# print(next(string_iterated))
+# print(next(string_iterated))
+# print(next(string_iterated))
+# print(next(string_iterated))
+#
 
-for char in string:
-    print(char)
+# mini challenge
+numbers = ['1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20']
+iter_number = iter(numbers)
 
-print('-------------')
-string_iterated = iter(string)
-print(string_iterated)
-print(next(string_iterated))
-print(next(string_iterated))
-print(next(string_iterated))
-print(next(string_iterated))
-print(next(string_iterated))
-print(next(string_iterated))
-print(next(string_iterated))
-print(next(string_iterated))
-print(next(string_iterated))
-print(next(string_iterated))
+i = ''
+x = len(numbers)
+
+for i in range(0, x):
+    print(next(iter_number))
+
+
