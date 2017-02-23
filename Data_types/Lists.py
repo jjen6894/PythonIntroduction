03ffe4aeb6f3505329c7e02cd0ca7e897b3f9d29
jjen6894 <jack_6894@hotmail.com>
2017-02-23 13:33:33 +0000

    difference between calling methods on objects to most other languages

--- Data_types/Lists.py
+++ Data_types/Lists.py
@@ -6,3 +6,12 @@ for state in parrot_list:
     print("The parrot is: {}".format(state))
 
 
+even = [2, 4, 6, 8]
+odd = [1, 3, 5, 7, 9]
+
+numbers = odd + even
+numbers_in_order = sorted(numbers)
+print(numbers)
+print(numbers_in_order)
+print(sorted(numbers))
+print(numbers.sort())
