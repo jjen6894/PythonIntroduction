70cb6ba35d02f5f0360705b21b172ece82f82bbd
jjen6894 <jack_6894@hotmail.com>
2017-02-23 13:31:57 +0000

    iterating through a list for alternative output

---
+++ Data_types/Lists.py
@@ -0,0 +1,8 @@
+parrot_list = ["non pinin' ", "no more", "a stiff", "bereft of life"]
+
+parrot_list.append(("A Noreweigen Blue"))
+
+for state in parrot_list:
+    print("The parrot is: {}".format(state))
+
+
