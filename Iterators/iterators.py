7adcca06a1f46636accd04a6477574a252bfeeae
jjen6894 <jack_6894@hotmail.com>
2017-02-23 14:10:37 +0000

    iterator function used elimantes the for loop first instance is the object

---
+++ Iterators/iterators.py
@@ -0,0 +1,18 @@
+string = '1234567890'
+
+for char in string:
+    print(char)
+
+print('-------------')
+string_iterated = iter(string)
+print(string_iterated)
+print(next(string_iterated))
+print(next(string_iterated))
+print(next(string_iterated))
+print(next(string_iterated))
+print(next(string_iterated))
+print(next(string_iterated))
+print(next(string_iterated))
+print(next(string_iterated))
+print(next(string_iterated))
+print(next(string_iterated))
