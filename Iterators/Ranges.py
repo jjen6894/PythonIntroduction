33f315054d9e5474f5ec91225853ca1133092183
jjen6894 <jack_6894@hotmail.com>
2017-02-24 12:27:49 +0000

    looking further at the values comparatively and looping negatively through a range

--- Iterators/Ranges.py
+++ Iterators/Ranges.py
@@ -1,3 +1,14 @@
 decimals = range(0, 100)
+print(decimals)
 my_range = decimals[3:40:3]
+print(my_range)
 print(my_range == range(3, 40, 3))
+print(list(range(0, 5, 2)))
+print(list(range(0, 6, 2)))
+
+r = decimals
+print(r)
+
+for i in r[::-2]:
+    print(i)
+
