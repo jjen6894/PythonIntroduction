9d428e474846fbe29e16036e2c49b9afaad87b41
jjen6894 <jack_6894@hotmail.com>
2017-02-27 17:18:24 +0000

    challenge completed looking to create the process on 7-10 12-15 in a for loop

--- Challenge/tuple_challenge.py
+++ Challenge/tuple_challenge.py
@@ -2,4 +2,15 @@ imelda = "More Mayhem", "Imilda May", 2011, (
     (1, "Pulling the Rug"), (2, "Psycho"), (3, "Mayhem"), (4, "Kentish Town Waltz"))
 
 title, artist, year, tracks = imelda
-print(tracks)
+
+(track_1, track_2, track_3, track_4) = tracks
+track_number_1, track_title_1 = track_1
+track_number_2, track_title_2 = track_2
+track_number_3, track_title_3 = track_3
+track_number_4, track_title_4 = track_4
+print(title, artist, year)
+print(track_number_1, ' ', track_title_1)
+print(track_number_2, ' ', track_title_2)
+print(track_number_3, ' ', track_title_3)
+print(track_number_4, ' \t', track_title_4)
+
