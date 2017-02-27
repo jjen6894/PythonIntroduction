eda62c6ab149a1b79b41f464555b86eceb582ab4
jjen6894 <jack_6894@hotmail.com>
2017-02-27 17:00:51 +0000

    tuple challenge started, list album details and indented song tracks

---
+++ Challenge/tuple_challenge.py
@@ -0,0 +1,5 @@
+imelda = "More Mayhem", "Imilda May", 2011, (
+    (1, "Pulling the Rug"), (2, "Psycho"), (3, "Mayhem"), (4, "Kentish Town Waltz"))
+
+title, artist, year, tracks = imelda
+print(tracks)
