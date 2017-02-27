4d7fa7a907bbc7eaded8d163ddee1509a1934c4d
jjen6894 <jack_6894@hotmail.com>
2017-02-27 16:57:56 +0000

    assigning tuples to multiple variables at once print multiple variables at once

---
+++ tuples/tuples2.py
@@ -0,0 +1,8 @@
+imelda = "More Mayhem", "Imilda May", 2011, (
+    (1, "Pulling the Rug"), (2, "Psycho"), (3, "Mayhem"), (4, "Kentish Town Waltz"))
+
+print(imelda)
+
+title, artist, year, tracks = imelda
+
+print(title, artist, year, tracks)
