c92c37304aee1d7fe07690f7f6513d50fb45be46
jjen6894 <jack_6894@hotmail.com>
2017-03-02 11:15:24 +0000

    add new key and value, saving new value overiding prevous one assigned to that key

--- Dictionaries/dictionaries.py
+++ Dictionaries/dictionaries.py
@@ -3,7 +3,13 @@ fruit = {"orange": "a sweet, orange, citrus fruit",
          "lemon": "a sour, yellow citrus fruit",
          "grape": "a small, sweet fruit growing in bunches",
          'lime': "a sour, green citrus fruit"}
+
 print(fruit)
 print(fruit['orange'])
 print(fruit['lime'])
 print(fruit['lemon'])
+fruit["pear"] = "an odd shaped apple"
+print(fruit)
+fruit["pear"] = "great with for cider"
+# existing key overight save the new value
+print(fruit)
--- Dictionaries/motorbike.py
+++ Dictionaries/motorbike.py
@@ -1,2 +1,4 @@
 bike = {"make": "Honda", "model": "250 dream", "colour": "red", "engine size": 250}
 print(bike['engine size'])
+print(bike['colour'])
+# good for storing attributes/properties of an object
