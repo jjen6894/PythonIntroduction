a44617da82ebaac41d059f2de616f7cf0c3eb868
jjen6894 <jack_6894@hotmail.com>
2017-03-02 11:24:42 +0000

    input from user using a while loop allowing them to call infomation from dictionary and present it to the user

--- Dictionaries/dictionaries.py
+++ Dictionaries/dictionaries.py
@@ -4,18 +4,29 @@ fruit = {"orange": "a sweet, orange, citrus fruit",
          "grape": "a small, sweet fruit growing in bunches",
          'lime': "a sour, green citrus fruit"}
 
-print(fruit)
-print(fruit['orange'])
-print(fruit['lime'])
-print(fruit['lemon'])
-fruit["pear"] = "an odd shaped apple"
-print(fruit)
+# print(fruit)
+# print(fruit['orange'])
+# print(fruit['lime'])
+# print(fruit['lemon'])
+# fruit["pear"] = "an odd shaped apple"
+# print(fruit)
 fruit["pear"] = "great with for cider"
 # existing key overight save the new value
-print(fruit)
+# print(fruit)
 
 
-del fruit["lemon"]
-print(fruit)
-fruit.clear()
-print(fruit)
+# del fruit["lemon"]
+# print(fruit)
+# fruit.clear()
+# print(fruit)
+while True:
+    dict_key = input("Please enter a fruit: ")
+    if dict_key == 'quit':
+        break
+    if dict_key in fruit:
+        description = fruit.get(dict_key)
+        print(description)
+    else:
+        if dict_key == '':
+            print("Please enter something")
+        print("We don't have a {}".format(dict_key))
