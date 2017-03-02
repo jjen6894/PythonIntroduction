02d95a1db1da78ee90796dbff6b72de5936838e6
jjen6894 <jack_6894@hotmail.com>
2017-03-02 11:52:00 +0000

    join method explored

---
+++ Dictionaries/join.py
@@ -0,0 +1,9 @@
+my_list = ["a", "b", "c", "d"]
+letters = 'abcdefghijklomnpqrstuvwxyz'
+new_string = ''
+new_string = ', '.join(my_list)
+string_2 = ', '.join(letters)
+
+
+print(new_string)
+print(string_2)
