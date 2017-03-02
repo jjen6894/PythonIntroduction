32a5fa727bfbcbd6f6b4f1d00c7e1d503cd5acd8
jjen6894 <jack_6894@hotmail.com>
2017-03-02 11:54:19 +0000

    location dictionary, start of game creation

--- Dictionaries/join.py
+++ Dictionaries/join.py
@@ -1,9 +1,16 @@
-my_list = ["a", "b", "c", "d"]
-letters = 'abcdefghijklomnpqrstuvwxyz'
-new_string = ''
-new_string = ', '.join(my_list)
-string_2 = ', '.join(letters)
+# my_list = ["a", "b", "c", "d"]
+# letters = 'abcdefghijklomnpqrstuvwxyz'
+# new_string = ''
+# new_string = ', '.join(my_list)
+# string_2 = ', '.join(letters)
+#
+#
+# print(new_string)
+# print(string_2)
 
-
-print(new_string)
-print(string_2)
+locations = {0: "You are sitting in front of a computer learning Python",
+             1: "You are standing at the end of a road before a small brick building",
+             2: "You are at the top of a hill",
+             3: "You are inside a building, a well house for a smalls stream",
+             4: "You are in a valley beside a steam",
+             5: "You are in the forest"}
