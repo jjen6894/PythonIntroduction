039dfb3418db8afc816133a80119d4571a3cb630
jjen6894 <jack_6894@hotmail.com>
2017-03-02 15:35:15 +0000

    intro to Sets

---
+++ Sets/sets.py
@@ -0,0 +1,13 @@
+farm_animals = {"sheep", "cow", "hen"}
+print(farm_animals)
+for animal in farm_animals:
+    print(animal)
+
+print("-"*50)
+
+wild_animals = {"Lion", "Tiger", "Panther", "Elephant", "Hare"}
+print(wild_animals)
+
+for animal in wild_animals:
+    print(animal)
+
