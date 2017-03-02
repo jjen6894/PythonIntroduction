8722145e7207046008551565eb6d94f849e0b861
jjen6894 <jack_6894@hotmail.com>
2017-03-02 14:06:36 +0000

    game setup copied most code across, added directions names as well

---
+++ Dictionaries/game.py
@@ -0,0 +1,48 @@
+locations = {0: "You are sitting in front of a computer learning Python",
+             1: "You are standing at the end of a road before a small brick building",
+             2: "You are at the top of a hill",
+             3: "You are inside a building, a well house for a smalls stream",
+             4: "You are in a valley beside a steam",
+             5: "You are in the forest"}
+
+exits = {0: {"Q": 0},
+         1: {"W": 2, "2": 2, "E": 3, "3": 3, "N": 5, "5": 5, "S": 4, "4": 4, "Q": 0},
+         2: {"N": 5, "5": 5, "Q": 0},
+         3: {"W": 1, "1": 1, "Q": 0},
+         4: {"N": 1, "1": 1, "W": 2, "2": 2, "Q": 0},
+         5: {"W": 2, "2": 2, "S": 1, "1": 1, "Q": 0}}
+
+direction_converter = {"NORTH": "N",
+                       "SOUTH": "S",
+                       "WEST": "W",
+                       "EAST": "E",
+                       "QUIT": "Q",
+                       "ROAD": "1",
+                       "HILL": "2",
+                       "BUILDING": "3",
+                       "VALLEY": "4",
+                       "FOREST": "5"}
+
+loc = 1
+while True:
+    available_exits = ', '.join(exits[loc].keys())
+
+    print(locations[loc])
+
+    if loc == 0:
+        break
+
+    direction = input("Available exits are " + available_exits.upper())
+    direction = direction.upper()
+    print()
+    if len(direction) > 1:
+        words = direction.split()
+        for word in words:
+            if word in direction_converter:
+                direction = direction_converter[word]
+                break
+
+    if direction in exits[loc]:
+        loc = exits[loc][direction]
+    else:
+        print("You cannot go in that direction")
