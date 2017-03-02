78550db95acee18e7096391587f761adb04df384
jjen6894 <jack_6894@hotmail.com>
2017-03-02 12:17:58 +0000

    first challenge completed change exits from list into just dictionary data type

--- Challenge/dictionary_challenge_1.py
+++ Challenge/dictionary_challenge_1.py
@@ -5,11 +5,13 @@ locations = {0: "You are sitting in front of a computer learning Python",
              3: "You are inside a building, a well house for a smalls stream",
              4: "You are in a valley beside a steam",
              5: "You are in the forest"}
-exits = [{"Q": 0},
-         {"W": 2, "E": 3, "N": 5, "S": 4, "Q": 0},
-         {"W": 1, "Q": 0},
-         {"N": 1, "W": 2, "Q": 0},
-         {"W": 2, "S": 1, "Q": 0}]
+
+exits = {0: {"Q": 0},
+         1: {"W": 2, "E": 3, "N": 5, "S": 4, "Q": 0},
+         2: {"N": 1, "Q": 0},
+         3: {"W": 1, "Q": 0},
+         4: {"N": 1, "W": 2, "Q": 0},
+         5: {"W": 2, "S": 1, "Q": 0}}
 
 loc = 1
 while True:
