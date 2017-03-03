be44905f2bbb36fffa8f78ccd8094e3a7a9d4135
jjen6894 <jack_6894@hotmail.com>
2017-03-03 10:11:10 +0000

    subtraction in sets difference method

--- Sets/sets.py
+++ Sets/sets.py
@@ -15,29 +15,46 @@
 # print(farm_animals)
 # print(wild_animals)
 
-empty_set = set()
-empty_set.add("a")
+# empty_set = set()
+# empty_set.add("a")
 # doesn't create a set makes a dictionary
 # empty_set_2 = {}
 # empty_set_2.add("a")
-
+#
+# even = set(range(0, 40, 2))
+# print(even)
+# print(len(even))
+#
+# squares_tuple = (4, 6, 9, 16, 25)
+# squares = set(squares_tuple)
+# print(squares)
+# print(len(squares))
+#
+# print(even.union(squares))
+# print(len(even.union(squares)))
+#
+# print(squares.union(even))
+#
+# print("-"*50)
+#
+# print(even.intersection(squares))
+# print(even & squares)
+# print(squares.intersection(even))
+# print(squares & even)
 even = set(range(0, 40, 2))
-print(even)
-print(len(even))
-
+print(sorted(even))
 squares_tuple = (4, 6, 9, 16, 25)
 squares = set(squares_tuple)
-print(squares)
-print(len(squares))
+print(sorted(squares))
 
-print(even.union(squares))
-print(len(even.union(squares)))
+print("even minus squares")
+print(sorted(even.difference(squares)))
+print(sorted(even - squares))
 
-print(squares.union(even))
+print("squares minus even")
+print(sorted(squares.difference(even)))
+print(sorted(squares - even))
 
-print("-"*50)
 
-print(even.intersection(squares))
-print(even & squares)
-print(squares.intersection(even))
-print(squares & even )
+
+
