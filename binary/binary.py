9b8b568c27236250d1b2c703a49826ee2623e462
jjen6894 <jack_6894@hotmail.com>
2017-03-03 14:29:17 +0000

    wrote two files in binary

--- binary/binary.py
+++ binary/binary.py
@@ -1,9 +1,21 @@
 # for i in range(1000):
 #     print("{0:>2} in binary is {0:>08b} ".format(i))
+#
+# with open("binary", "bw") as bin_file:
+#     bin_file.write(bytes(range(17)))
+#
+# with open("binary", 'br') as bin_file:
+#     for b in bin_file:
+#         print(b)
 
-with open("binary", "bw") as bin_file:
-    bin_file.write(bytes(range(17)))
+a = 65534 # FF FE
+b = 65535  #FF FF
+c = 65536   # 00 01 00 00
+x = 2998302 # 02 2D C0 1E
 
-with open("binary", 'br') as bin_file:
-    for b in bin_file:
-        print(b)
+with open("binary2", "bw") as bin_file:
+    bin_file.write(a.to_bytes(2, 'big'))
+    bin_file.write(b.to_bytes(2, 'big'))
+    bin_file.write(c.to_bytes(4, 'big'))
+    bin_file.write(x.to_bytes(4, 'big'))
+    bin_file.write(x.to_bytes(4, 'little'))
