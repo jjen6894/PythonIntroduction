6d99f82aa4bda019e5ba692a2cda439edef2f518
jjen6894 <jack_6894@hotmail.com>
2017-03-03 10:06:48 +0000

    sets addition concatenation the & method is calling intersection

--- Sets/sets.py
+++ Sets/sets.py
@@ -1,13 +1,43 @@
-farm_animals = {"sheep", "cow", "hen"}
-print(farm_animals)
-for animal in farm_animals:
-    print(animal)
+# farm_animals = {"sheep", "cow", "hen"}
+# print(farm_animals)
+# for animal in farm_animals:
+#     print(animal)
+#
+# print("-"*50)
+#
+# wild_animals = {"Lion", "Tiger", "Panther", "Elephant", "Hare"}
+# print(wild_animals)
+#
+# for animal in wild_animals:
+#     print(animal)
+# farm_animals.add("horse")
+# wild_animals.add("horse")
+# print(farm_animals)
+# print(wild_animals)
+
+empty_set = set()
+empty_set.add("a")
+# doesn't create a set makes a dictionary
+# empty_set_2 = {}
+# empty_set_2.add("a")
+
+even = set(range(0, 40, 2))
+print(even)
+print(len(even))
+
+squares_tuple = (4, 6, 9, 16, 25)
+squares = set(squares_tuple)
+print(squares)
+print(len(squares))
+
+print(even.union(squares))
+print(len(even.union(squares)))
+
+print(squares.union(even))
 
 print("-"*50)
 
-wild_animals = {"Lion", "Tiger", "Panther", "Elephant", "Hare"}
-print(wild_animals)
-
-for animal in wild_animals:
-    print(animal)
-
+print(even.intersection(squares))
+print(even & squares)
+print(squares.intersection(even))
+print(squares & even )
