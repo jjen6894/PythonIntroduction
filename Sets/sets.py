25f37eb1ae88978bf055423f0d0d2f7f6570333b
jjen6894 <jack_6894@hotmail.com>
2017-03-03 10:31:54 +0000

    frozen set created immuteable

--- Sets/sets.py
+++ Sets/sets.py
@@ -1,77 +1,71 @@
-# farm_animals = {"sheep", "cow", "hen"}
-# print(farm_animals)
-# for animal in farm_animals:
-#     print(animal)
-#
-# print("-"*50)
-#
-# wild_animals = {"Lion", "Tiger", "Panther", "Elephant", "Hare"}
-# print(wild_animals)
-#
-# for animal in wild_animals:
-#     print(animal)
-# farm_animals.add("horse")
-# wild_animals.add("horse")
-# print(farm_animals)
-# print(wild_animals)
+farm_animals = {"sheep", "cow", "hen"}
+print(farm_animals)
+for animal in farm_animals:
+    print(animal)
 
-# empty_set = set()
-# empty_set.add("a")
+print("-"*50)
+
+wild_animals = {"Lion", "Tiger", "Panther", "Elephant", "Hare"}
+print(wild_animals)
+
+for animal in wild_animals:
+    print(animal)
+farm_animals.add("horse")
+wild_animals.add("horse")
+print(farm_animals)
+print(wild_animals)
+
+empty_set = set()
+empty_set.add("a")
 # doesn't create a set makes a dictionary
-# empty_set_2 = {}
-# empty_set_2.add("a")
-#
-# even = set(range(0, 40, 2))
-# print(even)
-# print(len(even))
-#
-# squares_tuple = (4, 6, 9, 16, 25)
-# squares = set(squares_tuple)
-# print(squares)
-# print(len(squares))
-#
-# print(even.union(squares))
-# print(len(even.union(squares)))
-#
-# print(squares.union(even))
-#
-# print("-"*50)
-#
-# print(even.intersection(squares))
-# print(even & squares)
-# print(squares.intersection(even))
-# print(squares & even)
-# even = set(range(0, 40, 2))
-# print(sorted(even))
-# squares_tuple = (4, 6, 9, 16, 25)
-# squares = set(squares_tuple)
-# print(sorted(squares))
-
-# print("even minus squares")
-# print(sorted(even.difference(squares)))
-# print(sorted(even - squares))
-#
-# print("squares minus even")
-# print(sorted(squares.difference(even)))
-# print(sorted(squares - even))
-#
-#
-# print("-"*59)
-#
-# print(sorted(even))
-# print(squares)
-# even.difference_update(squares)
-# print(sorted(even))
+empty_set_2 = {}
+empty_set_2.add("a")
 
 even = set(range(0, 40, 2))
 print(even)
+print(len(even))
+
 squares_tuple = (4, 6, 9, 16, 25)
 squares = set(squares_tuple)
 print(squares)
-# print("symmetric even minus squares")
-# print(even.symmetric_difference(squares))
-# print("symmetric squares minus even")
-# print(squares.symmetric_difference(even))
+print(len(squares))
+
+print(even.union(squares))
+print(len(even.union(squares)))
+
+print(squares.union(even))
+
+print("-"*50)
+
+print(even.intersection(squares))
+print(even & squares)
+print(squares.intersection(even))
+print(squares & even)
+even = set(range(0, 40, 2))
+print(sorted(even))
+squares_tuple = (4, 6, 9, 16, 25)
+squares = set(squares_tuple)
+print(sorted(squares))
+
+print("even minus squares")
+print(sorted(even.difference(squares)))
+print(sorted(even - squares))
+
+print("squares minus even")
+print(sorted(squares.difference(even)))
+print(sorted(squares - even))
+
+
+print("-"*59)
+
+print(sorted(even))
+print(squares)
+even.difference_update(squares)
+print(sorted(even))
+print("symmetric even minus squares")
+print(even.symmetric_difference(squares))
+print("symmetric squares minus even")
+print(squares.symmetric_difference(even))
 squares.discard(4)
 squares.remove(16)
 squares.discard(8)
@@ -80,3 +74,20 @@ try:
     squares.remove(8)
 except KeyError:
     print("The item 8 is not a member of the set")
+
+even = set(range(0, 40, 2))
+print(even)
+squares_tuple = (4, 6, 16)
+squares = set(squares_tuple)
+print(squares)
+
+if squares.issubset(even):
+    print("squares is a subset of even")
+
+if even.issuperset(squares):
+    print("even is a superset of squares")
+
+# frozen set immuteable version of a set
+even = frozenset(range(0, 100, 2))
+
+print(even)
