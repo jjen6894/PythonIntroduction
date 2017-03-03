665233fb48fd1d47ed14f80a7728dc8b0b9543c1
jjen6894 <jack_6894@hotmail.com>
2017-03-03 14:08:37 +0000

    writing and reading binary files

--- binary/binary.py
+++ binary/binary.py
@@ -1,4 +1,9 @@
-for i in range(1000):
-    print("{0:>2} in binary is {0:>08b} ".format(i))
-    
+# for i in range(1000):
+#     print("{0:>2} in binary is {0:>08b} ".format(i))
 
+with open("binary", "bw") as bin_file:
+    bin_file.write(bytes(range(17)))
+
+with open("binary", 'br') as bin_file:
+    for b in bin_file:
+        print(b)
