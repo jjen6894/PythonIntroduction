33955fe8ce4579ba97ada2ea6d16035641bb1364
jjen6894 <jack_6894@hotmail.com>
2017-03-03 14:36:26 +0000

    read the file, see that thevalue that switched from little back to big returned a different value

--- binary/binary.py
+++ binary/binary.py
@@ -19,3 +19,20 @@ with open("binary2", "bw") as bin_file:
     bin_file.write(c.to_bytes(4, 'big'))
     bin_file.write(x.to_bytes(4, 'big'))
     bin_file.write(x.to_bytes(4, 'little'))
+
+
+with open("binary2", "br") as bin_file:
+    e = int.from_bytes(bin_file.read(2), 'big')
+    print(e)
+    f = int.from_bytes(bin_file.read(2), 'big')
+    print(f)
+    g = int.from_bytes(bin_file.read(4), 'big')
+    print(g)
+    h = int.from_bytes(bin_file.read(4), 'big')
+    print(h)
+    i = int.from_bytes(bin_file.read(4), 'big')
+    print(i)
+
+
+
+
