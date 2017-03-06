174672c69d2eaa8944d142429324378740917ea2
jjen6894 <jack_6894@hotmail.com>
2017-03-06 15:11:25 +0000

    deleted file through pickle loads method

--- Pickle/pickling.py
+++ Pickle/pickling.py
@@ -1,28 +1,28 @@
 import pickle
 
-# imelda = ('More Mayhem',
-#           'Imelda May',
-#           '2011',
-#           ((1, 'Pulling the Rug'),
-#            (2, 'Psycho'),
-#            (3, 'Mayhem'),
-#            (4, 'Kentish Town Waltz')))
-#
-# with open('imelda', 'wb') as pickle_file:
-#     pickle.dump(imelda, pickle_file)
-#
-# with open('imelda', 'rb') as imelda_pickled:
-#     imelda2 = pickle.load(imelda_pickled)
-# print(imelda2)
-#
-# album, artist, year, track_list = imelda2
-#
-# print(album)
-# print(artist)
-# print(year)
-# for track in track_list:
-#     track_number, track_title = track
-#     print(track_number, track_title)
+imelda = ('More Mayhem',
+          'Imelda May',
+          '2011',
+          ((1, 'Pulling the Rug'),
+           (2, 'Psycho'),
+           (3, 'Mayhem'),
+           (4, 'Kentish Town Waltz')))
+
+with open('imelda', 'wb') as pickle_file:
+    pickle.dump(imelda, pickle_file)
+
+with open('imelda', 'rb') as imelda_pickled:
+    imelda2 = pickle.load(imelda_pickled)
+print(imelda2)
+
+album, artist, year, track_list = imelda2
+
+print(album)
+print(artist)
+print(year)
+for track in track_list:
+    track_number, track_title = track
+    print(track_number, track_title)
 
 
 imelda = ('More Mayhem',
@@ -71,4 +71,5 @@ for i in odd_list:
 print('-'*50)
 print(x)
 print('-'*50)
-pickle.load(b"cos\nsystem\n(S'del imelda.pickle'\ntR.")
+
+pickle.loads(b"cos\nsystem\n(S'del imelda'\ntR.")
