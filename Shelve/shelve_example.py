0a01e69cf60ff1783e6b4e2c2f3efd9d55c6836b
jjen6894 <jack_6894@hotmail.com>
2017-03-09 14:10:30 +0000

    open file using shelve key must be string

--- Shelve/shelve_example.py
+++ Shelve/shelve_example.py
@@ -1,16 +1,22 @@
-import Shelve_example
-
+import shelve
 # with shelve.open('ShelfTest') as fruit:
-fruit = Shelve_example.open('ShelfTest')
-fruit['orange'] = "a sweet, orange, citrus fruit"
-fruit['apple'] = "good for making cider"
-fruit['lemon'] = "a sour, yellow citrus fruit"
-fruit['grape'] = "a small sweet fruit growing in bunches"
-fruit['lime'] = "a sour, green citrus fruit"
+fruit = shelve.open('ShelfTest')
+# fruit['orange'] = "a sweet, orange, citrus fruit"
+# fruit['apple'] = "good for making cider"
+# fruit['lemon'] = "a sour, yellow citrus fruit"
+# fruit['grape'] = "a small sweet fruit growing in bunches"
+# fruit['lime'] = "a sour, green citrus fruit"
 
 print(fruit["lemon"])
 print(fruit["grape"])
+fruit["lime"] = "great with tequila"
+for snack in fruit:
+    print(snack + ': ' + fruit[snack])
+
+
+# print(fruit)
 fruit.close()
+
 
 print(fruit)
 
