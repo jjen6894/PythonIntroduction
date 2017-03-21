bed38750447ffe832ea8271ceb94c89c08daffc3
jjen6894 <jack_6894@hotmail.com>
2017-03-21 14:15:41 +0000

    pytz module downloaded and used

---
+++ dateandtime/pytz.py
@@ -0,0 +1,10 @@
+import datetime
+import pytz
+
+country = 'Europe/Moscow'
+
+tz_to_display = pytz.timezone(country)
+local_time = datetime.datetime.now(tz=tz_to_display)
+print("The time in {} is {}".format(country, local_time))
+print("UTC time is {}".format(datetime.datetime.utcnow()))
+
